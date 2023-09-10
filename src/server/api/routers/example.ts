47d0da00e4b80b8be989e550544e426c35e7947c
Acrylic125 <acrylic125email@gmail.com>
2023-09-10 09:22:46 +0800

    chore: add deps

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { prisma } from "@/server/db";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
