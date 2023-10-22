87bb9bf741aa635c2ffafc4a7590071835af48fe
Acrylic125 <acrylic125email@gmail.com>
2023-10-22 16:05:03 +0800

    feat: tailwind config resolver

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {} as Record<"sm" | "md" | "lg", string>,
   },
   plugins: [],
 } satisfies Config;
