eca4bc5a6946ecb8b03ab399e36edc8a050b6cc0
Acrylic125 <acrylic125email@gmail.com>
2023-11-18 22:03:58 +0800

    feat: remove overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider cookies={cookies().toString()}>
-          <main className="relative h-screen overflow-auto">
+          <main className="relative h-screen">
             <MainNavbar className="sticky top-0" />
             {children}
           </main>
