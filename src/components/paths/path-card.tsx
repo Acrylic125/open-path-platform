328e3a01c370da0980cfc1653b5dfdcdeab3b66c
Acrylic125 <acrylic125email@gmail.com>
2023-11-18 22:04:58 +0800

    feat: path card component

---
+++ src/components/paths/path-card.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export default function PathCard({
+  id,
+  image,
+  location,
+  subLocation,
+  className,
+}: {
+  id: string;
+  image: string;
+  location: string;
+  subLocation: string[];
+  className?: string;
+}) {
+  return (
+    <Card
+      tabIndex={0}
+      className={cn(
+        className,
+        "flex h-fit overflow-hidden hover:border-foreground focus:border-foreground md:flex-col",
+      )}
+    >
+      <CardHeader className="flex-1 p-0">
+        <div className="relative aspect-video h-full w-full">
+          <Image src={image} alt="Image" fill />
+        </div>
+      </CardHeader>
+      <CardContent className="flex flex-1 flex-col gap-2 p-4">
+        <CardTitle className="text-base md:text-lg">{location}</CardTitle>
+        <CardDescription className="text-sm md:text-base">
+          {subLocation.join(", ")}
+        </CardDescription>
+      </CardContent>
+    </Card>
+  );
+}
