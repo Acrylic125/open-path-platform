8f2cd74e5c35be57d4fab1c36442d4e29bc120ab
Acrylic125 <acrylic125email@gmail.com>
2023-11-19 09:18:59 +0800

    refactor: clean up uis

--- src/app/paths/[pathId]/page.tsx
+++ src/app/paths/[pathId]/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
-import AutoSizerScrollView from "@/components/utils/auto-sizer-scroll-view";
-import StartPathLayout from "@/components/paths/start-path-layout";
 import PathCard from "@/components/paths/path-card";
 
 const paths: {
@@ -21,23 +19,42 @@ for (let i = 0; i < 20; i++) {
 }
 
 export default function Page() {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
-  // In the desktop view, there are 2 sections, the image preview and the main content.
-  // The content may be scrollable but the image preview needs to stay put. Thus, we need
-  // to use an overflow-auto to isolate this scrolling region.
-  //
-  // CSS overflow auto requires a fixed height (e.g. 240px, 100vh).
-  // We want the height of the starting position list container to take up the remaining
-  // height of the page. Thus, in order to calculate this, we take the current offset y position
-  // from the total height of the page.
-
   return (
     <div className="flex flex-1 flex-col items-center">
-      {/* <h1>Hello</h1> */}
-
-      <StartPathLayout className="relative overflow-auto">
-        {/* <div className="grid grid-cols-1 gap-2 p-4 md:gap-4 md:px-6"> */}
+      <div
+        className="relative h-[var(--safe-content-height)] w-full max-w-7xl overflow-auto border-l border-r"
+      >
+        <div className="w-full transition-all duration-0 ease-in-out">
+          <section className="flex h-36 justify-end bg-black text-white sm:h-40 md:h-48">
+            <div className="relative h-full w-full max-w-7xl">
+              <Image
+                src="https://images.unsplash.com/photo-1682685797660-3d847763208e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                alt="Image"
+                className="object-cover"
+                fill
+              />
+              <div className="absolute flex h-full w-full flex-col justify-end bg-gradient-to-t from-black/100 to-black/0 to-75%">
+                <div className="p-4 md:p-6">
+                  <h1 className="scroll-m-20 pb-2 text-xl font-bold tracking-tight transition-colors first:mt-0 lg:text-3xl">
+                    FC6
+                  </h1>
+                  <p className="text-sm text-muted">
+                    Singapore Polytechnic, Singapore, Singapore
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="sticky top-0 z-10 flex w-full flex-row items-center justify-between gap-4 border-b border-t bg-background px-4 py-2 md:px-6 md:py-4">
+          <h1 className="w-full text-sm font-semibold md:text-base lg:text-lg">
+            Where do you want to start from?
+          </h1>
+          <Input
+            placeholder="Search for a starting point"
+            className="max-w-md"
+          />
+        </div>
         <div className="grid grid-cols-1 gap-3 px-4 py-2 pb-16 md:grid-cols-2 md:px-6 md:py-4 lg:grid-cols-3 xl:grid-cols-4">
           {paths.map((p) => (
             <PathCard
@@ -49,7 +66,7 @@ export default function Page() {
             />
           ))}
         </div>
-      </StartPathLayout>
+      </div>
     </div>
   );
 }
--- src/app/paths/page.tsx
+++ src/app/paths/page.tsx
@@ -1,51 +1,5 @@
+import PathCard from "@/components/paths/path-card";
 import { Button } from "@/components/ui/button";
-import { MainNavbar } from "@/components/main/main-navbar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-
-function PathCard({
-  id,
-  image,
-  location,
-  subLocation,
-}: {
-  id: string;
-  image: string;
-  location: string;
-  subLocation: string[];
-}) {
-  return (
-    <Card
-      tabIndex={0}
-      className="flex overflow-hidden hover:border-foreground focus:border-foreground md:flex-col"
-    >
-      <CardHeader className="flex-1 p-0">
-        <div className="relative aspect-video h-full w-full">
-          <Image
-            src="https://images.unsplash.com/photo-1682685797660-3d847763208e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-            alt="Image"
-            fill
-          />
-        </div>
-      </CardHeader>
-      <CardContent className="flex flex-1 flex-col gap-2 p-4">
-        <CardTitle className="text-base md:text-lg">{location}</CardTitle>
-        <CardDescription className="text-sm md:text-base">
-          {subLocation.join(", ")}
-        </CardDescription>
-      </CardContent>
-      {/* <CardFooter className="border-t-2 p-4">
-                  <p>View</p>
-                </CardFooter> */}
-    </Card>
-  );
-}
 
 const paths: {
   id: string;
@@ -67,30 +21,29 @@ export default function Home() {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <>
-      <div className="flex w-full flex-col items-center">
-        <section className="flex w-full max-w-7xl flex-col gap-3 px-4 py-4 md:gap-6 md:px-8 md:py-8">
-          <header className="w-full">
-            <h1 className="scroll-m-19 m:text-3xl text-2xl font-bold first:mt-0">
-              Paths
-            </h1>
-          </header>
-          <div className="grid grid-cols-1 gap-3 pb-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {paths.map((p) => (
-              <PathCard
-                key={p.id}
-                id={p.id}
-                image={p.image}
-                location={p.location}
-                subLocation={p.subLocation}
-              />
-            ))}
-          </div>
-        </section>
-      </div>
+    <div className="flex w-full flex-1 flex-col items-center">
+      <section className="relative flex h-[var(--safe-content-height)] w-full max-w-7xl flex-col overflow-auto">
+        <header className="sticky top-0 z-10 w-full border-b border-t bg-background px-4 py-2 md:gap-6 md:px-6 md:py-4">
+          <h1 className="w-full text-sm font-semibold md:text-base lg:text-lg">
+            Paths
+          </h1>
+        </header>
+        <div className="grid grid-cols-1 gap-2 px-4 py-2 pb-16 md:grid-cols-2 md:gap-4 md:px-6 md:py-4 lg:grid-cols-3 xl:grid-cols-4">
+          {paths.map((p) => (
+            <PathCard
+              key={p.id}
+              id={p.id}
+              image={p.image}
+              location={p.location}
+              subLocation={p.subLocation}
+              href={`/paths/${p.id}`}
+            />
+          ))}
+        </div>
+      </section>
       <div className="fixed bottom-6 right-6 z-50 flex w-fit shadow-2xl md:hidden">
         <Button size="lg">New Path</Button>
       </div>
-    </>
+    </div>
   );
 }
